c23b3c23e2e393b1a67bb907de579b0373ec42aa
joshporter <jporter@sessiondigital.com>
2014-08-05 12:19:23 +0100

    Fix travis issues

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 Magento BDD Playground
 ===
 [![Build Status](https://travis-ci.org/joshporter/magento-bdd-playground.svg?branch=master)](https://travis-ci.org/joshporter/magento-bdd-playground)
+
 Magento CE 1.8.1 (1.9 sample data is massive). Install behat and phpspec with relevant extensions.
 
 ```bash
@@ -14,7 +15,7 @@ Add following line to `/etc/hosts`:
 192.168.50.50 magento-bdd.dev
 ```
 
-To run behat test, need to be inside of VM
+To run tests, need to be inside of VM
 
 ```bash
 cd tools
--- tools/travis_setup.sh
+++ tools/travis_setup.sh
@@ -8,7 +8,7 @@ echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php
 ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
 
 # Configure Apache
-WEBROOT="$(pwd)/vendor/magetest/magento/src"
+WEBROOT="$(pwd)/public"
 sudo echo "<VirtualHost *:80>
   DocumentRoot $WEBROOT
 
@@ -32,7 +32,7 @@ sudo echo "<VirtualHost *:80>
 sudo service apache2 restart
 
 # Configure custom domain
-echo "127.0.0.1 manager.dev" | sudo tee --append /etc/hosts
+echo "127.0.0.1 magento-bdd.dev" | sudo tee --append /etc/hosts
 
 # Install Magento sample data
 wget -O magento-sample-data-1.6.1.0.tar.gz http://www.magentocommerce.com/downloads/assets/1.6.1.0/magento-sample-data-1.6.1.0.tar.gz
@@ -41,4 +41,4 @@ mysql -uroot -e 'CREATE DATABASE 'magento';'
 mysql -uroot magento < magento-sample-data-1.6.1.0/magento_sample_data_for_1.6.1.0.sql
 
 # Install Magento CE 1.8
-php -f vendor/magetest/magento/src/install.php -- --license_agreement_accepted yes --locale en_GB --timezone Europe/London --default_currency GBP --db_host localhost --db_name magento --db_user root --db_pass "" --url http://manager.dev/ --skip_url_validation yes --use_rewrites yes --use_secure no --secure_base_url --use_secure_admin no --admin_firstname admin --admin_lastname admin --admin_email admin@example.com --admin_username admin --admin_password adminadmin123123
+php -f public/install.php -- --license_agreement_accepted yes --locale en_GB --timezone Europe/London --default_currency GBP --db_host localhost --db_name magento --db_user root --db_pass "" --url http://manager.dev/ --skip_url_validation yes --use_rewrites yes --use_secure no --secure_base_url http://manager.dev/ --use_secure_admin no --admin_firstname admin --admin_lastname admin --admin_email admin@example.com --admin_username admin --admin_password 123123pass
